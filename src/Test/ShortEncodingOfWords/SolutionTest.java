747befadfb145407366bd14dbfc732ec4663e1f4
tsund <tsund@qq.com>
2020-03-28 10:03:46 +0800

    add solution 820

---
+++ src/Test/ShortEncodingOfWords/SolutionTest.java
@@ -0,0 +1,21 @@
+package ShortEncodingOfWords;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author: shenke
+ * @project: LeetCode
+ * @date: 2020/3/28
+ */
+
+public class SolutionTest {
+
+    @Test
+    public void minimumLengthEncoding() {
+
+        Solution solution = new Solution();
+        solution.minimumLengthEncoding(null);
+    }
+}
