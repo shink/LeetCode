58e646e1105eaf2453174f81c9564c947480f08c
tsund <tsund@qq.com>
2020-05-12 12:24:58 +0800

    add solution 155

---
+++ src/Simple/MinStack/Solution.java
@@ -0,0 +1,54 @@
+package MinStack;
+
+import java.util.Stack;
+
+/**
+ * @author: shenke
+ * @project: LeetCode
+ * @date: 2020/5/12
+ * @desp: 155题——最小栈, https://leetcode-cn.com/problems/min-stack/
+ */
+
+public class Solution {
+
+}
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+
+    public void push(int x) {
+        //当前值更小
+        if (x <= min) {
+            //将之前的最小值保存
+            stack.push(min);
+            //更新最小值
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        //如果弹出的值是最小值，那么将下一个元素更新为最小值
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
