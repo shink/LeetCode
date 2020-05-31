71b17896bff278c33f0fab92484404639526aa1d
tsund <tsund@qq.com>
2020-05-31 21:20:24 +0800

    add solution 101

---
+++ src/Simple/SymmetricTree/Solution.java
@@ -0,0 +1,38 @@
+package SymmetricTree;
+
+/**
+ * @author: shenke
+ * @project: LeetCode
+ * @date: 2020/5/31
+ * @desp: 101题——对称二叉树, https://leetcode-cn.com/problems/symmetric-tree/
+ */
+
+public class Solution {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+
+        return check(root.left, root.right);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+}
