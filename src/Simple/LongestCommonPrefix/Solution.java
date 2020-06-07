56a26eeee38120298619afb32303416b7cf2283f
tsund <tsund@qq.com>
2020-06-07 16:07:21 +0800

    update solution 14

--- src/Simple/LongestCommonPrefix/Solution.java
+++ src/Simple/LongestCommonPrefix/Solution.java
@@ -1,14 +1,27 @@
 package LongestCommonPrefix;
 
+/**
+ * @author: shenke
+ * @project: LeetCode
+ * @date: 2020/6/7
+ * @desp: 14——最长公共前缀, https://leetcode-cn.com/problems/longest-common-prefix/
+ */
+
 public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
 
-	public String longestCommonPrefix(String[] strs) {
+        String res = strs[0];
 
-		String result = "";
-		
-		
-		
-		return result;
-	}
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(res) != 0) {
+                res = res.substring(0, res.length() - 1);
+                if (res.isEmpty())
+                    return "";
+            }
+        }
 
+        return res;
+    }
 }
