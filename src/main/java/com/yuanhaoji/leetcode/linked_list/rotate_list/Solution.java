f4194d94e5bb47d9314edddb9c1b4921436702c2
tsund <tsund@qq.com>
2021-03-27 22:29:00 +0800

    add linked list 61

---
+++ src/main/java/com/yuanhaoji/leetcode/linked_list/rotate_list/Solution.java
@@ -0,0 +1,52 @@
+package com.yuanhaoji.leetcode.linked_list.rotate_list;
+
+/**
+ * @author: shenke
+ * @project: LeetCode
+ * @date: 2021/3/18
+ * @desp: 61中等题——旋转链表，https://leetcode-cn.com/problems/rotate-list/
+ */
+
+public class Solution {
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null) {
+            return head;
+        }
+        int n = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            iter = iter.next;
+            n++;
+        }
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+        iter.next = head;
+        while (add-- > 0) {
+            iter = iter.next;
+        }
+        ListNode ret = iter.next;
+        iter.next = null;
+        return ret;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
